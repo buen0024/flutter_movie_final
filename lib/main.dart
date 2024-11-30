677038030fea79376380a8314b8454b452c29b35
JanineAnn Buenaseda <buen0024@algonquinlive.com>
2024-11-30 17:10:01 -0500

    Add theme and routing to welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'themes/app_theme.dart';
+import 'screens/welcome_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: 'Movie Night!',
+      theme: AppTheme.themeData,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => WelcomeScreen(),
+      },
     );
   }
 }
