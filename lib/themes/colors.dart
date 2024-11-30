cf1231cbb0a8ef564e253218127e484599b6b909
JanineAnn Buenaseda <buen0024@algonquinlive.com>
2024-11-30 17:09:25 -0500

    Add colors for theme

---
+++ lib/themes/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF0F111A);
+  static const Color secondary = Color(0xFFAB4459);
+  static const Color background = Color(0xFF1A1C26);
+  static const Color surface = Color(0xFF2B2D3C);
+  static const Color onPrimary = Colors.white;
+  static const Color onSecondary = Colors.white;
+  static const Color onBackground = Colors.white70;
+  static const Color onSurface = Colors.white;
+  static const Color error = Color(0xFFF44336);
+}
