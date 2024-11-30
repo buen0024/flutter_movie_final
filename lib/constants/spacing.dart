38dc4b41baa9b0bef2c0c3c6c87c9f12bd96a2ee
JanineAnn Buenaseda <buen0024@algonquinlive.com>
2024-11-30 17:23:13 -0500

    Add spacing

---
+++ lib/constants/spacing.dart
@@ -0,0 +1,6 @@
+class AppSpacing {
+  static const double base = 8.0;
+  static const double small = base; // 8.0
+  static const double medium = base * 2; // 16.0
+  static const double large = base * 4; // 32.0
+}
