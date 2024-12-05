6926639b5dbae93c0dee5a91109a36f463c0a483
JanineAnn Buenaseda <buen0024@algonquinlive.com>
2024-12-05 18:47:23 -0500

    Navigate to movie selection, add env

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'themes/app_theme.dart';
 import 'screens/welcome_screen.dart';
 import 'screens/share_code_screen.dart';
 import 'screens/enter_code_screen.dart';
+import 'screens/movie_selection_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MainApp());
 }
 
@@ -21,6 +24,7 @@ class MainApp extends StatelessWidget {
         '/': (context) => WelcomeScreen(),
         '/shareCode': (context) => ShareCodeScreen(),
         '/enterCode': (context) => EnterCodeScreen(),
+        '/movieSelection': (context) => MovieSelectionScreen(),
       },
     );
   }
